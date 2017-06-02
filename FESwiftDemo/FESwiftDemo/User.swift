84cf7d1186d6627f5cba491c2809b3822bdedf2a
FlyElephant <keso_xiang@talkmate.com>
2017-06-02 18:01:53 +0800

    JSDemo 测试~

---
+++ FESwiftDemo/FESwiftDemo/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  FESwiftDemo
+//
+//  Created by FlyElephant on 2017/6/2.
+//  Copyright © 2017年 FlyElephant. All rights reserved.
+//
+
+import Foundation
+
+open class User:NSObject {
+    
+    dynamic func buyCourse() {
+        print("FlyElephant---通过Swift购买图书")
+    }
+}
---
+++ FESwiftDemo/FESwiftDemo/main.js
@@ -0,0 +1,7 @@
+
+
+defineClass('FESwiftDemo.User', {
+            buyCourse: function() {
+            console.log('FlyElephant---通过JS购买课程')
+            }
+            })
